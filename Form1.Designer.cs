e3ac8df53c7f3e703efd28df67affd0c2a433e2b
SumairMemon <67095920+SumairMemon@users.noreply.github.com>
2023-10-15 11:35:39 -0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace GenCertificate
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelIssuer = new System.Windows.Forms.Label();
+            this.textBoxIssuer = new System.Windows.Forms.TextBox();
+            this.textBoxSubject = new System.Windows.Forms.TextBox();
+            this.labelSubject = new System.Windows.Forms.Label();
+            this.labelValid = new System.Windows.Forms.Label();
+            this.numericUpDownMonths = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxFolderName = new System.Windows.Forms.TextBox();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMonths)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelIssuer
+            // 
+            this.labelIssuer.AutoSize = true;
+            this.labelIssuer.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelIssuer.Location = new System.Drawing.Point(7, 22);
+            this.labelIssuer.Name = "labelIssuer";
+            this.labelIssuer.Size = new System.Drawing.Size(51, 20);
+            this.labelIssuer.TabIndex = 0;
+            this.labelIssuer.Text = "Issuer";
+            // 
+            // textBoxIssuer
+            // 
+            this.textBoxIssuer.Location = new System.Drawing.Point(170, 19);
+            this.textBoxIssuer.Name = "textBoxIssuer";
+            this.textBoxIssuer.Size = new System.Drawing.Size(302, 27);
+            this.textBoxIssuer.TabIndex = 1;
+            this.textBoxIssuer.Text = "CN=MYCOMPANY ROOT CA";
+            // 
+            // textBoxSubject
+            // 
+            this.textBoxSubject.Location = new System.Drawing.Point(170, 72);
+            this.textBoxSubject.Name = "textBoxSubject";
+            this.textBoxSubject.Size = new System.Drawing.Size(596, 27);
+            this.textBoxSubject.TabIndex = 3;
+            this.textBoxSubject.Text = "C=GB, ST=Berkshire, L=Reading, O=MY COMPANY";
+            // 
+            // labelSubject
+            // 
+            this.labelSubject.AutoSize = true;
+            this.labelSubject.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelSubject.Location = new System.Drawing.Point(7, 75);
+            this.labelSubject.Name = "labelSubject";
+            this.labelSubject.Size = new System.Drawing.Size(60, 20);
+            this.labelSubject.TabIndex = 2;
+            this.labelSubject.Text = "Subject";
+            // 
+            // labelValid
+            // 
+            this.labelValid.AutoSize = true;
+            this.labelValid.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelValid.Location = new System.Drawing.Point(7, 134);
+            this.labelValid.Name = "labelValid";
+            this.labelValid.Size = new System.Drawing.Size(68, 20);
+            this.labelValid.TabIndex = 4;
+            this.labelValid.Text = "Valid for";
+            // 
+            // numericUpDownMonths
+            // 
+            this.numericUpDownMonths.Location = new System.Drawing.Point(170, 131);
+            this.numericUpDownMonths.Maximum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.numericUpDownMonths.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownMonths.Name = "numericUpDownMonths";
+            this.numericUpDownMonths.Size = new System.Drawing.Size(62, 27);
+            this.numericUpDownMonths.TabIndex = 5;
+            this.numericUpDownMonths.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(236, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "months";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(7, 206);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Output Folder";
+            // 
+            // textBoxFolderName
+            // 
+            this.textBoxFolderName.Location = new System.Drawing.Point(170, 203);
+            this.textBoxFolderName.Name = "textBoxFolderName";
+            this.textBoxFolderName.ReadOnly = true;
+            this.textBoxFolderName.Size = new System.Drawing.Size(393, 27);
+            this.textBoxFolderName.TabIndex = 8;
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Location = new System.Drawing.Point(583, 203);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(94, 29);
+            this.buttonBrowse.TabIndex = 9;
+            this.buttonBrowse.Text = "Browse...";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonBrowse);
+            this.groupBox1.Controls.Add(this.textBoxFolderName);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.numericUpDownMonths);
+            this.groupBox1.Controls.Add(this.labelValid);
+            this.groupBox1.Controls.Add(this.textBoxSubject);
+            this.groupBox1.Controls.Add(this.labelSubject);
+            this.groupBox1.Controls.Add(this.textBoxIssuer);
+            this.groupBox1.Controls.Add(this.labelIssuer);
+            this.groupBox1.Location = new System.Drawing.Point(14, 11);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(778, 269);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.Enabled = false;
+            this.buttonGenerate.Location = new System.Drawing.Point(581, 308);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(94, 29);
+            this.buttonGenerate.TabIndex = 11;
+            this.buttonGenerate.Text = "Generate";
+            this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(698, 308);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(94, 29);
+            this.buttonExit.TabIndex = 12;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(805, 349);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonGenerate);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Everything Embedded - X509 Certificate Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMonths)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelIssuer;
+        private System.Windows.Forms.TextBox textBoxIssuer;
+        private System.Windows.Forms.TextBox textBoxSubject;
+        private System.Windows.Forms.Label labelSubject;
+        private System.Windows.Forms.Label labelValid;
+        private System.Windows.Forms.NumericUpDown numericUpDownMonths;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxFolderName;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonGenerate;
+        private System.Windows.Forms.Button buttonExit;
+    }
+}
+
